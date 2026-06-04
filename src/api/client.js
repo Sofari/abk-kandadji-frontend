f61314a85562b1492dfc44c68465c1fcc17da104
Sofari <indice76@gmail.com>
2026-06-04 13:14:32 +0100

    Update API URL for production

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 
+  process.env.NODE_ENV === 'production'
+    ? 'https://abk-kandadji-api.onrender.com'
+    : 'http://localhost:3000';
 
 // Créer l'instance axios
 const client = axios.create({
